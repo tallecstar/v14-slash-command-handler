91defb94f763a06d29d3cfe767aa67028586cb64
tallecstar <85768859+tallecstar@users.noreply.github.com>
2022-11-19 19:27:50 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "prefix": "/",
-  "owner": "690639157013381220",
-  "token": "Bot Tokeniniz"
+  "owner": "141172792329502720",
+  "token": OTM1ODc5MDEyNzA0NzE0ODEz.GpTLfs.Zr9KIA46ZQZbJlFT6ZgJWRcY9mPdGFNT9LajiE"
 }
